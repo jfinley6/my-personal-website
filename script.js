bf4d07bea24391ce2d6b80c5b7ce8008c11d1478
John Finley <johnfinleybremerton@gmail.com>
2022-06-15 13:34:20 -0600

    Added an about page

--- script.js
+++ script.js
@@ -1,45 +1,45 @@
 //Declare variables for Toggle Button and Menu Item Elements
 const toggleButton = document.getElementsByClassName("toggle-button")[0];
-const menuItem = document.getElementsByClassName("menuItem")
-
+const menuItem = document.getElementsByClassName("menuItem");
 
 //When the toggle button is clicked display the menu items
-toggleButton.addEventListener('click', () => {
+toggleButton.addEventListener("click", () => {
     for (let i = 0; i < menuItem.length; i++) {
-        menuItem[i].classList.toggle("active")
+        menuItem[i].classList.toggle("active");
     }
-})
+});
+
+// searchElements = document.getElementsByClassName("searchElement");
+
+// let searchArray = [...searchElements];
 
 // function checkTags() {
-//     //Declare variables
-//     let input, container, filter, ul, li, i, txtValue;
 
+//     arr = [];
 //     input = document.getElementById("myInput");
 //     filter = input.value.toUpperCase();
-//     ul = document.getElementById("myUL");
-//     li = document.getElementsByClassName("liTag");
-//     container = document.getElementsByClassName("container")
-
-//     let searchArray = [];
-
-//     for (i = 0; i < container.length; i++) {
-//         for (i = 1; i < container[i].children[2].children.length; i++) {
-//             txtValue = container[i].children[2].children[i].innerText;
-//             searchArray.push(txtValue);
-//             console.log(searchArray)
+//     for (let i = 0; i < searchArray.length; i++) {
+//         arr.push(searchArray[i].innerText.replaceAll(",", "").toUpperCase());
+//         if (arr[i].includes(filter)) {
+//             searchArray[i].parentNode.parentNode.style.display = "";
+//         } else {
+//             searchArray[i].parentNode.parentNode.style.display = "none";
 //         }
+
 //     }
 // }
-// container = document.getElementsByClassName("container")
-// let txtValue = container[0].children[2].children[1].innerText
-// console.log(txtValue)
 
-// Declare variables 
+// arr = [];
+// input = document.getElementById("myInput");
+// filter = input.value.toUpperCase();
+// for (let i = 0; i < searchArray.length; i++) {
+//     arr.push(searchArray[i].innerText.replaceAll(",", "").toUpperCase());
+//     if (arr[i].includes(filter)) {
+//         searchArray[i].parentNode.parentNode.style.display = "";
+//     } else {
+//         searchArray[i].parentNode.parentNode.style.display = "none";
+//     }
 
-
-
-
-
-
-
-
+// }
+// console.log(arr)
+// console.log(arr[0].includes("H"))
